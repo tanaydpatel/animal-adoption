a4bf7aaa8e619e2ba484a8bb0a016231c471d66f
Tanay Patel <tanaydpatel@gmail.com>
2020-08-27 12:31:40 +0530

    converted to JSX,added SearchParam file and used hooks,added frontendmasters/pet for searchParams dropdown menu (via parcel)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", { id: "myParentDiv" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Jimmy",
-      animal: "Dog",
-      breed: "Desi",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-    React.createElement(Pet, {
-      name: "Peppet",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-  ]);
+  //   return React.createElement("div", { id: "myParentDiv" }, [
+  //     React.createElement("h1", {}, "Adopt Me!"),
+  //     React.createElement(Pet, {
+  //       name: "Simba",
+  //       animal: "Dog",
+  //       breed: "Labrador",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Doink",
+  //       animal: "Cat",
+  //       breed: "Mixed",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Peppet",
+  //       animal: "Bird",
+  //       breed: "Cockatiel",
+  //     }),
+  //   ]);
+
+  return (
+    <div id="myParentDiv">
+      <h1></h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,20 @@
 import React from "react";
 export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, name),
+  //     React.createElement("h2", {}, animal),
+  //     React.createElement("h2", {}, breed),
+  //   ]);
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
+/*
+Note :  To use JSX we need to have **React** in scope 
+        Because everything is being transpiled to React.createElement()   
+*/
